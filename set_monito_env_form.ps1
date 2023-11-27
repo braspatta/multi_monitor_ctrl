ce6c68f6301bbdf5ecb4d443f5df03cef5e749e7
Bras Patta <35237569+braspatta@users.noreply.github.com>
2023-11-27 09:08:01 +0000

    Initial commit

---
+++ set_monito_env_form.ps1
@@ -0,0 +1,118 @@
+# Load necessary assemblies
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+
+
+$profiles = @(
+    @(
+        [PSCustomObject]@{
+            Name     = 'All'
+            Command = {
+                    Start-Process multimonitortool-x64\MultiMonitorTool.exe -ArgumentList "/LoadConfig All.cfg"
+                }
+        },
+        [PSCustomObject]@{
+            Name     = 'S'
+            Command = {
+                    Start-Process multimonitortool-x64\MultiMonitorTool.exe -ArgumentList "/SaveConfig All.cfg"
+                }
+        }
+    ),
+    @(
+        [PSCustomObject]@{
+            Name     = 'Laptop'
+            Command = {
+                    # Start-Process multimonitortool-x64\MultiMonitorTool.exe -ArgumentList "/LoadConfig Laptop.cfg"
+                    Start-Process multimonitortool-x64\MultiMonitorTool.exe -ArgumentList "/disable \\.\DISPLAY5"
+                    Start-Process multimonitortool-x64\MultiMonitorTool.exe -ArgumentList "/disable \\.\DISPLAY9"
+                    Start-Process multimonitortool-x64\MultiMonitorTool.exe -ArgumentList "/disable \\.\DISPLAY6"
+                }
+        },
+        [PSCustomObject]@{
+            Name     = 'S'
+            Command = {
+                    Start-Process multimonitortool-x64\MultiMonitorTool.exe -ArgumentList "/SaveConfig Laptop.cfg"
+                }
+        }
+    ),
+    @(
+        [PSCustomObject]@{
+            Name     = 'Laptop Top'
+            Command = {
+                    # Start-Process multimonitortool-x64\MultiMonitorTool.exe -ArgumentList "/LoadConfig LaptopTop.cfg"
+                    Start-Process multimonitortool-x64\MultiMonitorTool.exe -ArgumentList "/disable \\.\DISPLAY5"
+                    Start-Process multimonitortool-x64\MultiMonitorTool.exe -ArgumentList "/enable \\.\DISPLAY9"
+                    Start-Process multimonitortool-x64\MultiMonitorTool.exe -ArgumentList "/disable \\.\DISPLAY6"
+                }
+        },
+        [PSCustomObject]@{
+            Name     = 'S'
+            Command = {
+                    Start-Process multimonitortool-x64\MultiMonitorTool.exe -ArgumentList "/SaveConfig LaptopTop.cfg"
+                }
+        }
+    ),
+    @(
+        [PSCustomObject]@{
+            Name     = 'Laptop Dell'
+            Command = {
+                    # Start-Process multimonitortool-x64\MultiMonitorTool.exe -ArgumentList "/LoadConfig LaptopDell.cfg"
+                    Start-Process multimonitortool-x64\MultiMonitorTool.exe -ArgumentList "/enable \\.\DISPLAY6"
+                    Start-Process multimonitortool-x64\MultiMonitorTool.exe -ArgumentList "/disable \\.\DISPLAY5"
+                    Start-Process multimonitortool-x64\MultiMonitorTool.exe -ArgumentList "/disable \\.\DISPLAY9"
+
+                }
+        },
+        [PSCustomObject]@{
+            Name     = 'S'
+            Command = {
+                    Start-Process multimonitortool-x64\MultiMonitorTool.exe -ArgumentList "/SaveConfig LaptopDell.cfg"
+                }
+        }
+    )
+)
+
+
+function MakeButton($form,$row, $col, $width, $height, $text, $command) {
+    $button = New-Object System.Windows.Forms.Button
+    $button.Location = New-Object System.Drawing.Point($col,$row)
+    $button.Size = New-Object System.Drawing.Size($width, $height)
+    $button.Text = $text
+    $button.Add_Click($command)
+    $form.Controls.Add($button)
+}
+
+$profileBtnWidth = 100
+$profileBtnHeight = 30
+$saveBtnWidth = 30
+$saveBtnHeight = 30
+$betweenProfGap = 30
+$betweenBtnfGap = 10
+
+# Create the form
+$form = New-Object System.Windows.Forms.Form
+$form.Text = 'Monitor env selection'
+$form.Size = New-Object System.Drawing.Size(750,100)
+$form.StartPosition = 'CenterScreen'
+
+For ($p=0; $p -lt $profiles.Length; $p++)
+{
+    $row = 10
+
+    $col = 10 + ($p * ($profileBtnWidth + $betweenBtnfGap + $saveBtnWidth + $betweenProfGap))
+    $actionBtn = $profiles[$p][0]
+    $saveBtn = $profiles[$p][1]
+
+    $save_col = $col + $profileBtnWidth + $betweenBtnfGap
+    MakeButton $form $row $col      $profileBtnWidth $profileBtnHeight  $actionBtn.Name $actionBtn.Command
+    MakeButton $form $row $save_col $saveBtnWidth    $saveBtnHeight     $saveBtn.Name   $saveBtn.Command
+}
+
+# # Show the form
+$form.ShowDialog()
+
+# # DISPLAY5 AOC
+# # DISPLAY9 TOP
+# # DISPLAY1 LAPTOP
+# # DISPLAY6 DELL
